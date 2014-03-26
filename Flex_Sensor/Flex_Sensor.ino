b9fa5e2f8475fb2868b9a16cef518c03c4a806f5
Meghan Jimenez <hyprmeghan@gmail.com>
2014-03-26 13:58:43 -0700

    Improved Accel Code

--- Flex_Sensor/Flex_Sensor.ino
+++ Flex_Sensor/Flex_Sensor.ino
@@ -15,26 +15,32 @@ Voltage divider with 15K resistor, 3.3V source
 void setup(){
   //initialize serial communications
  Serial.begin(9600); 
+ pinMode(8,OUTPUT);
+ 
 }
 
-int downThresh1 = 350;  //point at which we assume the finger is down
+int downThresh1 = 378;  //point at which we assume the finger is down
 boolean down1 = false; //straight at 400, bent value depends on where you bend
                        //we need to put these in the glove to determine final values
 
 void loop(){
  //Read sensor, print value
- int sensor, degrees;
+ int sensor1;
  sensor1 = analogRead(0);
  Serial.print(sensor1); 
   
 //Check if the finger is down  
 if (sensor1 < downThresh1){
  down1 = true;
+ digitalWrite(8,HIGH);
  Serial.print("Finger 1 is down");
  Serial.println();
 }
 else
+{
  down1 = false;
+digitalWrite(8,HIGH);
+}
  
  //print and delay for next read
  Serial.println();
